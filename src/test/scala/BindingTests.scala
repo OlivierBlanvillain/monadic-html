cbde906a67fe88bd75f9981099b4e9a6d59fcf73
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2016-10-11 08:50:52 +0200

    Explicit s.tick() in tests

--- src/test/scala/BindingTests.scala
+++ src/test/scala/BindingTests.scala
@@ -6,7 +6,7 @@ import org.scalatest.FunSuite
 import scala.xml.Elem
 
 class BindingTests extends FunSuite {
-  implicit val s = TestScheduler()
+  implicit val s: TestScheduler = TestScheduler()
 
   test("Mounting Elem") {
     val div = dom.document.createElement("div")
@@ -27,11 +27,14 @@ class BindingTests extends FunSuite {
     val text: Var[String] = Var("original text")
     val span: Elem = <span><p>pre {text} <br/> post </p></span>
     val div = dom.document.createElement("div")
-    mount(div, span); s.tick()
+    mount(div, span)
+    s.tick()
     assert(div.innerHTML == "<span><p>pre original text <br> post </p></span>")
-    text := "changed"; s.tick()
+    text := "changed"
+    s.tick()
     assert(div.innerHTML == "<span><p>pre changed <br> post </p></span>")
-    text := "changed again"; s.tick()
+    text := "changed again"
+    s.tick()
     assert(div.innerHTML == "<span><p>pre changed again <br> post </p></span>")
   }
 
@@ -39,11 +42,16 @@ class BindingTests extends FunSuite {
     val list: Var[Seq[String]] = Var(Seq("original text 0", "original text 1"))
     val span: Elem = <span> <p> { list.map(xs => for (x <- xs) yield <b>{x}</b>) } </p> </span>
     val div = dom.document.createElement("div")
-    mount(div, span); s.tick()
+    mount(div, span)
+    s.tick()
     assert(div.innerHTML == "<span> <p> <b>original text 0</b><b>original text 1</b> </p> </span>")
-    list.update("prepended" +: _); s.tick(); s.tick()
+    list.update("prepended" +: _)
+    s.tick()
+    s.tick()
     assert(div.innerHTML == "<span> <p> <b>prepended</b><b>original text 0</b><b>original text 1</b> </p> </span>")
-    list.update(_.patch(1, Nil, 1)); s.tick(); s.tick() // Remove element as position 1
+    list.update(_.patch(1, Nil, 1)) // Remove element as position 1
+    s.tick()
+    s.tick()
     assert(div.innerHTML == "<span> <p> <b>prepended</b><b>original text 1</b> </p> </span>")
   }
 
@@ -51,9 +59,11 @@ class BindingTests extends FunSuite {
     val id: Var[String] = Var("oldId")
     val hr: Binding[Elem] = id.map(i => <hr id={i}/>)
     val div = dom.document.createElement("div")
-    mount(div, hr); s.tick()
+    mount(div, hr)
+    s.tick()
     assert(div.innerHTML == """<hr id="oldId">""")
-    id := "newId"; s.tick()
+    id := "newId"
+    s.tick()
     assert(div.innerHTML == """<hr id="newId">""")
   }
 
@@ -78,12 +88,6 @@ class BindingTests extends FunSuite {
           lastName.toLowerCase.contains(pattern)
       }
 
-    // Node: If using cats/scalaz, you might as well replace this boilerplate with the following instance:
-    // implicit val BindingMonadInstance: cats.Monad[Binding] = new cats.Monad[Binding] {
-    //   def pure[A](x: A): Binding[A] = apply(x)
-    //   def flatMap[A, B](fa: Binding[A])(f: A => Binding[B]): Binding[B] = fa.flatMap(f)
-    //   def tailRecM[A, B](a: A)(f: (A) => Binding[Either[A, B]]): Binding[B] = defaultTailRecM(a)(f)
-    // }
     implicit class SequencingSeqFFS[A](self: Seq[Binding[A]]) {
       def sequence: Binding[Seq[A]] =
         self.foldRight(Binding(Seq[A]()))(for {n<-_;s<-_} yield n+:s)
@@ -109,11 +113,19 @@ class BindingTests extends FunSuite {
       <table><thead><tr><td>First Name</td><td>Second Name</td><td>Age</td></tr></thead>{tbodyBinding}</table>
 
     val div = dom.document.createElement("div")
-    mount(div, tableBinding); s.tick(); s.tick(); s.tick(); s.tick()
+    mount(div, tableBinding)
+    s.tick()
+    s.tick()
+    s.tick()
+    s.tick()
 
     assert(div.innerHTML == """<table><thead><tr><td>First Name</td><td>Second Name</td><td>Age</td></tr></thead><tbody><tr><td>Steve</td><td>Jobs</td><td>10</td></tr><tr><td>Tim</td><td>Cook</td><td>12</td></tr><tr><td>Jeff</td><td>Lauren</td><td>13</td></tr></tbody></table>""")
 
-    filterPattern := "o"; s.tick(); s.tick(); s.tick(); s.tick()
+    filterPattern := "o"
+    s.tick()
+    s.tick()
+    s.tick()
+    s.tick()
 
     assert(div.innerHTML == """<table><thead><tr><td>First Name</td><td>Second Name</td><td>Age</td></tr></thead><tbody><tr><td>Steve</td><td>Jobs</td><td>10</td></tr><tr><td>Tim</td><td>Cook</td><td>12</td></tr></tbody></table>""")
   }
@@ -122,9 +134,11 @@ class BindingTests extends FunSuite {
     val child = Var[Elem](<hr/>)
     val parent = <p><span>{child}</span><span>{child}</span></p>
     val div = dom.document.createElement("div")
-    mount(div, parent); s.tick()
+    mount(div, parent)
+    s.tick()
     assert(div.innerHTML == "<p><span><hr></span><span><hr></span></p>")
-    child := <br/>; s.tick()
+    child := <br/>
+    s.tick()
     assert(div.innerHTML == "<p><span><br></span><span><br></span></p>")
   }
 
