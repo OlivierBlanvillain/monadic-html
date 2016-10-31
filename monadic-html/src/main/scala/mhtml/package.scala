094964ce90969f5e610c78d0f98d2ff22c34edd4
Ólafur Páll Geirsson <olafurpg@gmail.com>
2016-10-31 22:29:03 +0100

    Clean up changes in mhtml.

--- monadic-html/src/main/scala/mhtml/package.scala
+++ monadic-html/src/main/scala/mhtml/package.scala
@@ -3,8 +3,7 @@ import scala.language.implicitConversions
 import scala.xml.Node
 
 package object mhtml {
-//  implicit def OptionToOSN[A](a: Option[A]): Option[Node]          = a.map(x => new Atom(x))
-  implicit def RxToOSN[A](a: Rx[A]): Seq[Node]                     = new xml.Atom(a)
-  implicit def Function0ToOSN(a: Function0[Unit]): Seq[Node]       = new xml.Atom(a)
-  implicit def Function1ToOSN[A](a: Function1[A, Unit]): Seq[Node] = new xml.Atom(a)
+  implicit def RxToOSN[A](a: Rx[A]): Node                     = new xml.Atom(a)
+  implicit def Function0ToOSN(a: Function0[Unit]): Node       = new xml.Atom(a)
+  implicit def Function1ToOSN[A](a: Function1[A, Unit]): Node = new xml.Atom(a)
 }
