636019db98629d64be1da98f106f331a4774e31e
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2016-12-17 01:48:36 +0100

    Update att/elm implicits to support None/Some
    Also add a paragraph of comment to explain this `C[x]` thing.

--- monadic-html/src/main/scala/scala/xml/xml.scala
+++ monadic-html/src/main/scala/scala/xml/xml.scala
@@ -1,10 +1,12 @@
 package scala.xml
 
+import language.higherKinds
+
 // XML Nodes ------------------------------------------------------------------
 
 // Nodes are now implemented as an idiomatic sealed hierarchy of case classes.
 // `Atom` was kept as a class for source compatibility (even if it's not used
-// directly in scalac paser). `NoteSeq` direpeared, `Node <:!< Seq[Node]`...
+// directly in scalac paser). `NoteSeq` disappeared, `Node <:!< Seq[Node]`...
 
 /** Trait representing XML tree. */
 sealed trait Node
@@ -95,20 +97,20 @@ The following types are supported:
 - Option[T] where T is XmlAttributeEmbeddable (None → remove from the DOM)
 """)
 trait XmlAttributeEmbeddable[T] { def toNode(e: T): Node }
-object XmlAttributeEmbeddable{
-  import language.higherKinds
-
+object XmlAttributeEmbeddable {
   @inline def instance[T](f: T => Node): XmlAttributeEmbeddable[T] =
     new XmlAttributeEmbeddable[T] { override def toNode(e: T): Node = f(e) }
   @inline def atom[T]: XmlAttributeEmbeddable[T] = instance[T](new Atom(_))
 
+  implicit val noneAttributeEmbeddable: XmlAttributeEmbeddable[None.type]         = atom[None.type]
+  implicit val nilAttributeEmbeddable: XmlAttributeEmbeddable[Nil.type]           = atom[Nil.type]
   implicit val booleanAttributeEmbeddable: XmlAttributeEmbeddable[Boolean]        = atom[Boolean]
   implicit val stringAttributeEmbeddable: XmlAttributeEmbeddable[String]          = instance[String](Text.apply)
   implicit val textNodeAttributeEmbeddable: XmlAttributeEmbeddable[Text]          = instance(identity)
   implicit val function0AttributeEmbeddable: XmlAttributeEmbeddable[() => Unit]   = atom[() => Unit]
   implicit def function1AttributeEmbeddable[T]: XmlAttributeEmbeddable[T => Unit] = atom[T => Unit]
-  implicit def optionStringAttributeEmbeddable[T:XmlAttributeEmbeddable]: XmlAttributeEmbeddable[Option[T]]        = atom[Option[T]]
-  implicit def rxAttributeEmbeddable[C[_] <: mhtml.Rx[_], T: XmlAttributeEmbeddable]: XmlAttributeEmbeddable[C[T]] = atom[C[T]]
+  implicit def optionAttributeEmbeddable[C[x] <: Option[x], T: XmlAttributeEmbeddable]: XmlAttributeEmbeddable[C[T]] = atom[C[T]]
+  implicit def rxAttributeEmbeddable[C[x] <: mhtml.Rx[x], T: XmlAttributeEmbeddable]: XmlAttributeEmbeddable[C[T]]   = atom[C[T]]
 }
 
 /** Evidence that T can be embedded in xml element position. */
@@ -122,12 +124,12 @@ The following types are supported:
 """)
 trait XmlElementEmbeddable[T] { def toNode(e: T): Node }
 object XmlElementEmbeddable {
-  import language.higherKinds
-
   @inline def instance[T](f: T => Node): XmlElementEmbeddable[T] =
     new XmlElementEmbeddable[T] { override def toNode(e: T): Node = f(e) }
   @inline def atom[T]: XmlElementEmbeddable[T] = instance[T](new Atom(_))
 
+  implicit val nilElementEmbeddable: XmlElementEmbeddable[Nil.type]      = atom[Nil.type]
+  implicit val noneElementEmbeddable: XmlElementEmbeddable[None.type]    = atom[None.type]
   implicit val intElementEmbeddable: XmlElementEmbeddable[Int]           = atom[Int]
   implicit val floatElementEmbeddable: XmlElementEmbeddable[Float]       = atom[Float]
   implicit val doubleElementEmbeddable: XmlElementEmbeddable[Double]     = atom[Double]
@@ -135,12 +137,27 @@ object XmlElementEmbeddable {
   implicit val charElementEmbeddable: XmlElementEmbeddable[Char]         = instance[Char](x => Text(x.toString))
   implicit val stringElementEmbeddable: XmlElementEmbeddable[String]     = instance[String](Text.apply)
   implicit def nodeElementEmbeddable[T <: Node]: XmlElementEmbeddable[T] = instance[T](identity)
-  implicit def optionElementEmbeddable[T: XmlElementEmbeddable]: XmlElementEmbeddable[Option[T]]             = atom[Option[T]]
-  implicit def seqElementEmbeddable[C[X] <: Seq[X], T <: Node]: XmlElementEmbeddable[C[T]]                   = instance[C[T]](Group.apply)
-  implicit def rxElementEmbeddable[C[_] <: mhtml.Rx[_], T: XmlElementEmbeddable]: XmlElementEmbeddable[C[T]] = atom[C[T]]
+  implicit def optionElementEmbeddable[C[x] <: Option[x], T: XmlElementEmbeddable]: XmlElementEmbeddable[C[T]] = atom[C[T]]
+
+  // Here we really want `C[x] <: Seq[x]` instead of `C[_] <: Seq[_]`. `x` is
+  // deliberately lower case to indicate it's not a normal type parameter: it
+  // has a very local scope. To understand why these two definitions are not
+  // equivalent, one needs to understand that in `C[_] <: Seq[_]`, both `_`
+  // have a different meaning. On the left hand side, `C[_]` means that `C` is
+  // a type constructor taking a single type argument of kind `*`. On the
+  // right hand side, the `_` in `Seq[_]` indicated a *wildcard* type: it's
+  // essentially a shorthand for `Seq[t] forSome { type t >: Nothing <: Any]
+  // }`, which is itself of kind `*`. To summarize, `C[_] <: Seq[_]` means a
+  // higher kinded type of kind `(* -> *)` which is a subtype of `Seq[Any]`
+  // (makes no sense), While `C[x] <: Seq[x]` means a higher kinded type of
+  // kind `(* -> *)` which, when instantiated with a type `x`, is a subtype of
+  // `Seq[x]` (what we actually want).
+
+  implicit def seqElementEmbeddable[C[x] <: Seq[x], T <: Node]: XmlElementEmbeddable[C[T]] = instance[C[T]](Group.apply)
+  implicit def rxElementEmbeddable[C[x] <: mhtml.Rx[x], T: XmlElementEmbeddable]: XmlElementEmbeddable[C[T]] = atom[C[T]]
 }
 
-// Internal structure used by scalac to create literals -----------------------
+/** Internal structure used by scalac to create literals */
 class NodeBuffer extends scala.collection.mutable.ArrayBuffer[Node] {
   def &+[A](e: A)(implicit ev: XmlElementEmbeddable[A]): NodeBuffer = { super.+=(ev.toNode(e)); this }
 }
--- tests/src/test/scala/mhtml/RenderTests.scala
+++ tests/src/test/scala/mhtml/RenderTests.scala
@@ -13,26 +13,27 @@ class RenderTests extends FunSuite {
   def check(node: xml.Node, expected: String): Unit =
     test(expected)(assert(render(node) == expected))
 
-  // well-typed elements
-  check(<div>{Rx(Option(1))}</div>                , "<div>1</div>")
-  check(<div>{Option(2)}</div>                    , "<div>2</div>")
-  check(<div>{'a'}</div>                          , "<div>a</div>")
-  check(<div>{3}</div>                            , "<div>3</div>")
-  check(<div>{4L}</div>                           , "<div>4</div>")
-  check(<div>{4.2d}</div>                         , "<div>4.2</div>")
-  check(<div>{5.0f}</div>                         , "<div>5</div>")
-  check(<div>{Rx(Option.empty[Int])}6</div>       , "<div>6</div>")
-  check(<div>{Option.empty[Int]}7</div>           , "<div>7</div>")
-  check(<div>{"string"}</div>                     , "<div>string</div>")
-  check(<div>{List(scala.xml.Comment("a"))}</div> , "<div><!--a--></div>")
+  // Well-typed elements
+  check(<div>{Rx(Some(1))}</div>            , "<div>1</div>")
+  check(<div>{Some(2)}</div>                , "<div>2</div>")
+  check(<div>{Some(<p></p>)}</div>          , "<div><p></p></div>")
+  check(<div>{None}</div>                   , "<div></div>")
+  check(<div>{'a'}</div>                    , "<div>a</div>")
+  check(<div>{3}</div>                      , "<div>3</div>")
+  check(<div>{4L}</div>                     , "<div>4</div>")
+  check(<div>{4.2d}</div>                   , "<div>4.2</div>")
+  check(<div>{5.0f}</div>                   , "<div>5</div>")
+  check(<div>{Rx(None)}6</div>              , "<div>6</div>")
+  check(<div>{None}7</div>                  , "<div>7</div>")
+  check(<div>{"string"}</div>               , "<div>string</div>")
+  check(<div>{List(xml.Comment("a"))}</div> , "<div><!--a--></div>")
 
-  // optional attribute
-  check(<form disabled={Rx(true)}>1</form>             , """<form disabled="">1</form>""")
-  check(<form disabled={Rx(false)}>1</form>            , """<form>1</form>""")
-  check(<form disabled={true}>2</form>                 , """<form disabled="">2</form>""")
-  check(<form disabled={false}>2</form>                , """<form>2</form>""")
-  check(<form disabled={Option("banana")}>3</form>     , """<form disabled="banana">3</form>""")
-  check(<form disabled={Option.empty[String]}>3</form> , """<form>3</form>""")
-  check(<form disabled={Option(Rx(true))}>4</form>     , """<form disabled="">4</form>""")
-
+  // Optional attributes
+  check(<form disabled={Rx(true)}>1</form>       , """<form disabled="">1</form>""")
+  check(<form disabled={Rx(false)}>1</form>      , """<form>1</form>""")
+  check(<form disabled={true}>2</form>           , """<form disabled="">2</form>""")
+  check(<form disabled={false}>2</form>          , """<form>2</form>""")
+  check(<form disabled={Some("banana")}>3</form> , """<form disabled="banana">3</form>""")
+  check(<form disabled={None}>3</form>           , """<form>3</form>""")
+  check(<form disabled={Some(Rx(true))}>4</form> , """<form disabled="">4</form>""")
 }
