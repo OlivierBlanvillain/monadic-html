855cf28869dd3aeadead565bd6fa517874312d84
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2017-02-14 14:21:19 +0100

    Add elm Signal methods to Rx

--- monadic-rx/shared/src/main/scala/mhtml/rx.scala
+++ monadic-rx/shared/src/main/scala/mhtml/rx.scala
@@ -1,28 +1,27 @@
 package mhtml
 
-/** Reactive value of type `A`.
-  * Automatically recalculation when one of it's dependencies is updated. */
+/** Reactive value of type `A`. Automatically recalculate on dependency update. */
 trait Rx[+A] {
-  /** Applies the side effecting function `f` to each element of this [[Rx]].
-    * Returns an [[Cancelable]] which can be used to cancel the subscription.
-    * Use with caution: not canceling `foreach` subscription on short lived
-    * [[Rx]]s will leak memory. */
+  /**
+   * Applies the side effecting function `f` to each update of this [[Rx]].
+   *
+   * Returns a [[Cancelable]] which can be used to cancel the subscription.
+   * Caution: not canceling disposable subscriptions leaks memory.
+   */
   def foreach(s: A => Unit): Cancelable
 
-  /** Alternative to `foreach` which is not triggered immediately,
-    * but only once the underlying value is updated. */
-  protected[mhtml] def foreachNext(s: A => Unit): Cancelable
-
-  /** Returns a new [[Rx]] that maps each element of this [[Rx]] via `f`. */
+  /** Returns a new [[Rx]] that maps each update of this [[Rx]] via `f`. */
   def map[B](f: A => B): Rx[B] =
-    new Var[B](f(value), self => foreachNext(self := f(_)))
+    Var.unsafeCreate[B](None)(self => this.foreach(self := f(_)))
 
-  /** Returns a new [[Rx]] that flat-maps each element of this [[Rx]] via `f`. */
+  /** Returns a new [[Rx]] that flat-maps each update of this [[Rx]] via `f`. */
   def flatMap[B](f: A => Rx[B]): Rx[B] = {
     var cc = Cancelable.empty
-    var fb: Rx[B] = f(value)
+    // fb is an optimization: it prevents un/resubscribing when f is a
+    // constant function, which appends all the time in applicative style.
+    var fb: Rx[B] = null
 
-    val result = new Var[B](fb.value, self => foreachNext { a =>
+    Var.unsafeCreate[B](None)(self => this.foreach { a =>
       val newFb = f(a)
       if (fb != newFb) {
         cc.cancel()
@@ -30,45 +29,150 @@ trait Rx[+A] {
         fb = newFb
       }
     })
-
-    cc = fb.foreach(result.:=)
-    result
   }
 
-  /** Returns the current value of this [[Rx]]. Using `value` in conjonction
-    * with `:=` is an anti-pattern which can lead to infinit cycles and
-    * other surprising side effects of the value propagation order. */
-  def value: A
+  /**
+   * Create a past-dependent [[Rx]]. Each update from the incoming [[Rx]] will
+   * be used to step the state forward. The outgoing [[Rx]] represents the
+   * current state. Note that in the general settings of dynamic dependency
+   * graph, `foldp` breaks referential transparency.
+   */
+  def foldp[B](seed: B)(step: (A, B) => B): Rx[B] = {
+    var b = seed
+    this.map { a =>
+      val next = step(a, b)
+      b = next
+      next
+    }
+  }
+
+  /**
+   * Returns a new [[Rx]] with updates fulfilling the predicate `fp`.
+   * If the first update is dropped, the `a` value is used instead.
+   *
+   * {{{
+   * val numbers: Rx[Int]
+   * val even: Rx[Int] = numbers.keepIf(_ % 2 == 0)(-1)
+   * // numbers => 0 0 3 4 5 6 ...
+   * // even    => 0 0   4   6 ...
+   * }}}
+   */
+  def keepIf[B >: A](f: B => Boolean)(b: B): Rx[B] =
+    Var.create[B](b)(self => this.foreach(a => if (f(a)) self := a else ()))
+
+  /**
+   * Returns a new [[Rx]] without updates fulfilling the predicate `f`.
+   * If the first update is dropped, the `a` value is used instead.
+   *
+   * {{{
+   * val numbers: Rx[Int]
+   * val even: Rx[Int] = numbers.dropIf(_ % 2 == 0)(-1)
+   * // numbers =>  0 0 3 4 5 6 ...
+   * // even    => -1   3   5   ...
+   * }}}
+   */
+  def dropIf[B >: A](f: B => Boolean)(a: B): Rx[B] = this.keepIf[B](!f(_))(a)
+
+  /**
+   * Returns a new [[Rx]] with updates where `f` is defined and mapped by `f`.
+   * If the first update is dropped, the `b` value is used instead.
+   */
+  def collect[B](f: PartialFunction[A, B])(b: B): Rx[B] =
+    Var.create[B](b)(self => this.foreach(x => if (f.isDefinedAt(x)) self := f(x) else ()))
+
+  /**
+   * Drop repeated value of this [[Rx]].
+   *
+   * {{{
+   * val numbers: Rx[Int]
+   * val noDups: Rx[Int] = numbers.dropRepeats
+   * // numbers => 0 0 3 3 5 5 5 4 ...
+   * // noDups  => 0   3   5     4 ...
+   * }}}
+   *
+   * Note: This could also be implemented in term of keepIf map and foldp.
+   */
+  def dropRepeats: Rx[A] =
+    Var.unsafeCreate[A](None)({ self =>
+      var previous: Option[A] = None
+      this.foreach { x =>
+        if (previous.forall(x.!=)) {
+          self := x
+          previous = Some(x)
+        }
+      }
+    })
+
+  /**
+   * Merge two [[Rx]] into one.
+   *
+   * {{{
+   * val r1: Rx[Int]
+   * val r2: Rx[Int]
+   * val merged: Rx[Int] = r1.merge(r2)
+   * // r1     => 0 8     3 ...
+   * // r2     => 1   4 3   ...
+   * // merged => 0 8 4 3 3 ...
+   * }}}
+   *
+   * If an update comes from either of the incoming [[Rx]], the outgoing [[Rx]]
+   * is updated. Upon creation, the current value from the first [[Rx]] if kept.
+   */
+  def merge[B >: A](rx: Rx[B]): Rx[B] =
+    Var.unsafeCreate[B](None)({ v =>
+      val c2 = rx.foreach(v.:=)
+      val c1 = this.foreach(v.:=)
+      c1.alsoCanceling(c2)
+    })
+
+  /** Sample from the second input every time an event occurs on the first input. */
+  def sampleOn[B](rb: Rx[B]): Rx[A] =
+    Var.unsafeCreate[A](None)({ out =>
+      var currentA: A = null.asInstanceOf[A]
+      val ca = this.foreach(currentA = _)
+      val cb = rb.foreach(_ => out := currentA)
+      ca.alsoCanceling(cb)
+    })
+
+  /** The current value of this [[Rx]]. */
+  def value: A = {
+    var v: Option[A] = None
+    foreach(a => v = Some(a)).cancel()
+    // This can never happen if using the default Rx/Var constructors and
+    // methods. The proof is a simple case analysis showing that every method
+    // preseves non emptiness. Var created with unsafeCreate Messing up with Var unsafe constructor
+    // or internal could lead to this exception.
+    def error = new NoSuchElementException("Requesting value of an empty Rx.")
+    v.getOrElse(throw error)
+  }
 }
 
 object Rx {
-  /** Creates a constant [[Rx]] from a value. */
+  /** Creates a constant [[Rx]]. */
   def apply[A](v: A): Rx[A] =
     new Rx[A] {
-      def value = v
-
-      def foreachNext(s: A => Unit): Cancelable = Cancelable.empty
-
       def foreach(f: A => Unit): Cancelable = {
-        f(value)
+        f(v)
         Cancelable.empty
       }
-
     }
 }
 
-class Var[A] (initialValue: A, register: Var[A] => Cancelable) extends Rx[A] {
+/** A smart variable that can be set manually. */
+class Var[A](initialValue: Option[A], register: Var[A] => Cancelable) extends Rx[A] {
   // Last computed value, retained to be sent to new subscribers as they come in.
-  protected var cacheElem: A = initialValue
+  protected var cacheElem: Option[A] = initialValue
   // Current registration to the feeding `Rx`, canceled whenever nobody's listening.
   protected var registration: Cancelable = Cancelable.empty
   // Mutable set of all currently subscribed functions, implementing with an `Array`.
   protected val subscribers = buffer.empty[A => Unit]
 
-  def value: A = cacheElem
-
-  override def foreachNext(s: A => Unit): Cancelable = {
+  def foreach(s: A => Unit): Cancelable = {
     if (subscribers.isEmpty) registration = register(this)
+    cacheElem match {
+      case Some(v) => s(v)
+      case None    =>
+    }
     subscribers += s
     Cancelable { () =>
       subscribers -= s
@@ -76,20 +180,13 @@ class Var[A] (initialValue: A, register: Var[A] => Cancelable) extends Rx[A] {
     }
   }
 
-  override def foreach(s: A => Unit): Cancelable = {
-    s(cacheElem)
-    foreachNext(s)
-  }
-
-  /** Sets the value of this [[Var]].
-    * This will automatically trigger recalculation of all the depending [[Rx]]s. */
+  /** Sets the value of this [[Var]]. Triggers recalculation of depending [[Rx]]s. */
   def :=(newValue: A): Unit = {
-    cacheElem = newValue
+    cacheElem = Some(newValue)
     subscribers.foreach(_(newValue))
   }
 
-  /** Updates the value of this [[Var]] with a mutation function.
-    * This will automatically trigger recalculation of all the depending [[Rx]]s. */
+  /** Updates the value of this [[Var]]. Triggers recalculation of depending [[Rx]]s. */
   def update(f: A => A): Unit =
     foreach(a => :=(f(a))).cancel()
 }
@@ -97,10 +194,30 @@ class Var[A] (initialValue: A, register: Var[A] => Cancelable) extends Rx[A] {
 object Var {
   /** Create a [[Var]] from an initial value. */
   def apply[A](initialValue: A): Var[A] =
-    new Var(initialValue, _ => Cancelable.empty)
+    new Var[A](Some(initialValue), _ => Cancelable.empty)
+
+  /**
+   * Create a [[Var]] from an initial value and a register function .
+   *
+   * The register function is called as soon as this Var becomes active.
+   * The returned [[Cancelable]] is called when the Var becomes inactive.
+   *
+   * Registration & cancellation might append as many time as the Var goes
+   * active/inactive. This mechanism is what prevents `flatMap` from leaking
+   * memory: make sure that everything created in the register is canceled.
+   */
+  def create[A](initialValue: A)(register: Var[A] => Cancelable): Var[A] =
+    new Var[A](Some(initialValue), register)
+
+  /**
+   * Unsafe version of `create`, for internal use only. Using this method
+   * might exposes your code to empty Rx, making `.value` calls unsafe.
+   */
+  def unsafeCreate[A](initialValue: Option[A])(register: Var[A] => Cancelable): Var[A] =
+    new Var[A](initialValue, register)
 }
 
-/** Action that can be used to cancel `foreach` subscription. */
+/** Action used to cancel `foreach` subscription. */
 final case class Cancelable(cancel: () => Unit) extends AnyVal {
   def alsoCanceling(c: Cancelable): Cancelable = Cancelable { () => cancel(); c.cancel() }
 }
--- tests/src/test/scala/mhtml/RxTests.scala
+++ tests/src/test/scala/mhtml/RxTests.scala
@@ -71,4 +71,202 @@ class RxTests extends FunSuite {
     b := 4
     assert((104, 105) == result)
   }
+
+  test("Referential transparency with map") {
+    val a: Var[Int] = Var(0)
+    val b: Rx[Int] = a.map(identity)
+    assert(b.value == 0)
+    assert(a.map(identity).value == 0)
+    a := 1
+    assert(b.value == 1)
+    assert(a.map(identity).value == 1)
+    a := 2
+    assert(b.value == 2)
+    assert(a.map(identity).value == 2)
+  }
+
+  test("Flatmap is optimised for applicative style") {
+    var cancels: Int = 0
+    val r1: Var[Int] = Var(0)
+    val r2: Var[Int] = Var.create[Int](0)(_ => Cancelable(() => cancels += 1))
+    val out: Rx[Int] = r1.flatMap(_ => r2)
+    out.foreach(_ => ())
+    r1 := 1
+    assert(cancels == 0)
+    assert(out.value == 0)
+    r2 := 1
+    assert(cancels == 0)
+    assert(out.value == 1)
+  }
+
+  test("max using foldp") {
+    val value: Var[Int] = Var(0)
+    val max: Rx[Int] = value.foldp(0)(_ max _)
+
+    max.foreach(_ => ())
+
+    assert(max.value == 0)
+    value := 3
+    assert(max.value == 3)
+    value := 2
+    assert(max.value == 3)
+    value := 5
+    assert(max.value == 5)
+  }
+
+  test("foldp is not referentially transparent") {
+    // This is the counter example from Controlling Time and Space:
+    // understanding the many formulations of FRP.
+
+    val clicks: Var[Unit] = Var(())
+
+    val clickCount: Rx[Int] =
+      clicks.foldp(0) { case (_, c: Int) => c + 1 }
+
+    clickCount.foreach(_ => ())
+
+    assert(clickCount.value == 1)
+    clicks := (())
+    assert(clickCount.value == 2)
+    clicks := (())
+    assert(clickCount.value == 3)
+
+    def clicksOrZero(c: Boolean): Rx[Int] =
+      if (c) clickCount
+      else Rx(0)
+
+    val t1 = clicksOrZero(false)
+    assert(t1.value == 0)
+
+    val t2 = clicksOrZero(true)
+    assert(t2.value == 3)
+
+    // Evidence that foldp expressions (clickCount) are not referentially
+    // transparent with dynamic FRP (with bounded memory):
+
+    def clicksOrZero2(c: Boolean): Rx[Int] =
+      if (c) clicks.foldp(0) { case (_, c: Int) => c + 1 }
+      else Rx(0)
+
+    val t3 = clicksOrZero2(false)
+    assert(t3.value == 0)
+
+    val t4 = clicksOrZero2(true)
+    assert(t4.value == 1) // This was == 3 before
+  }
+
+  test("keepIf") {
+    val numbers: Var[Int] = Var(0)
+    val even: Rx[Int] = numbers.keepIf(_ % 2 == 0)(-1)
+    var numbersList: List[Int] = Nil
+    var evenList: List[Int] = Nil
+    numbers.foreach(n => numbersList = numbersList :+ n)
+    even.foreach(n => evenList = evenList :+ n)
+    numbers := 0
+    numbers := 3
+    numbers := 4
+    numbers := 5
+    numbers := 6
+    assert(numbersList == List(0, 0, 3, 4, 5, 6))
+    assert(evenList == List(0, 0, 4, 6))
+  }
+
+  test("keepIf fallback") {
+    val numbers: Var[Int] = Var(0)
+    val empty: Rx[Int] = numbers.keepIf(_ => false)(-1)
+    assert(empty.value == -1)
+    numbers := 1
+    assert(empty.value == -1)
+  }
+
+  test("dropIf") {
+    val numbers: Var[Int] = Var(0)
+    val even: Rx[Int] = numbers.dropIf(_ % 2 == 0)(-1)
+    var numbersList: List[Int] = Nil
+    var evenList: List[Int] = Nil
+    numbers.foreach(n => numbersList = numbersList :+ n)
+    even.foreach(n => evenList = evenList :+ n)
+    numbers := 0
+    numbers := 3
+    numbers := 4
+    numbers := 5
+    numbers := 6
+    assert(numbersList == List(0, 0, 3, 4, 5, 6))
+    assert(evenList == List(-1, 3, 5))
+  }
+
+  test("collect") {
+    val numbers: Var[Int] = Var(0)
+    val even: Rx[Int] = numbers.collect { case x if x % 2 == 0 => x * 10 } (-1)
+    var numbersList: List[Int] = Nil
+    var evenList: List[Int] = Nil
+    numbers.foreach(n => numbersList = numbersList :+ n)
+    even.foreach(n => evenList = evenList :+ n)
+    numbers := 2
+    numbers := 3
+    numbers := 4
+    numbers := 5
+    numbers := 6
+    assert(numbersList == List(0, 2, 3, 4, 5, 6))
+    assert(evenList == List(0, 20, 40, 60))
+  }
+
+  test("collect fallback") {
+    val numbers: Var[Int] = Var(0)
+    val empty: Rx[Int] = numbers.collect { case x if false => x }(-1)
+    assert(empty.value == -1)
+    numbers := 1
+    assert(empty.value == -1)
+  }
+
+  test("dropRepeats") {
+    val numbers: Var[Int] = Var(0)
+    val noDups: Rx[Int] = numbers.dropRepeats
+    var numbersList: List[Int] = Nil
+    var noDupsList: List[Int] = Nil
+    numbers.foreach(n => numbersList = numbersList :+ n)
+    noDups.foreach(n => noDupsList = noDupsList :+ n)
+    numbers := 0
+    numbers := 3
+    numbers := 3
+    numbers := 5
+    numbers := 5
+    numbers := 5
+    numbers := 4
+    assert(numbersList == List(0, 0, 3, 3, 5, 5, 5, 4))
+    assert(noDupsList == List(0, 3, 5, 4))
+  }
+
+  test("merge") {
+    val r1: Var[Int] = Var(0)
+    val r2: Var[Int] = Var(1)
+    val merged: Rx[Int] = r1.merge(r2)
+    var r1List: List[Int] = Nil
+    var r2List: List[Int] = Nil
+    var mergedList: List[Int] = Nil
+    r1.foreach(n => r1List = r1List :+ n)
+    r2.foreach(n => r2List = r2List :+ n)
+    merged.foreach(n => mergedList = mergedList :+ n)
+    r1 := 8
+    r2 := 4
+    r2 := 3
+    r1 := 3
+    assert(r1List == List(0, 8, 3))
+    assert(r2List == List(1, 4, 3))
+    assert(mergedList == List(0, 8, 4, 3, 3))
+  }
+
+  test("merge update") {
+    val r1: Var[Int] = Var(0)
+    val r2: Var[Int] = Var(1)
+    val merged: Rx[Int] = r1.merge(r2)
+    merged.foreach(_ => ())
+    assert(merged.value == 0)
+    r2 := 2
+    assert(merged.value == 2)
+    r1 := 3
+    assert(merged.value == 3)
+    r2 := 4
+    assert(merged.value == 4)
+  }
 }
